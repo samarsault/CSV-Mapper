50ba871fa1d0d99883d7a96ae1180a1a51745f50
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-24 15:23:05 +0530

    Update version after bug fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from distutils.core import setup
 
 setup(name='csvmapper',
-      version='0.21',
+      version='0.22',
       description='CSV parser using pre-defined mapper file',
       author='Samarjeet Singh',
       author_email='hi@samarjeet.me',
