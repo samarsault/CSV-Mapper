c0c324e6130106a6aac0663cad501836ddaed0eb
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-24 15:10:52 +0530

    Update simple example

--- examples/simple/simple.py
+++ examples/simple/simple.py
@@ -1,11 +1,11 @@
 import csvmapper
 
 csv_file = 'simple_record.csv'
-mapper = 'simple_mapper.csv'
+mapper = csvmapper.XMLMapper('simple_mapper.xml')
 parser = csvmapper.CSVParser(csv_file, mapper)
 
 items = parser.buildObject()
 
 for item in items:
-	print '{ Name:%s, Job:%s, Age:%d }\n' %(item.Name, item.Job, item.Age)
+	print '{ Name:%s, Job:%s, Age:%d }' %(item.Name, item.Job, item.Age)
 	# ^^ Note %d is used for age
