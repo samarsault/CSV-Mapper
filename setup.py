97777663e439b51f8d9ff147a61f4c9d6f52ee6c
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-24 14:06:47 +0530

    Improved setup.py info

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@
 from distutils.core import setup
 
 setup(name='csvmapper',
-      version='1.0',
+      version='0.1',
       description='CSV parser using pre-defined mapper file',
       author='Samarjeet Singh',
       author_email='hi@samarjeet.me',
       url='https://github.com/samarjeet27/CSV-Mapper',
-      packages=['csvmapper'],
+      download_url = 'https://codeload.github.com/samarjeet27/CSV-Mapper/zip/master',
+      keywords= ['csv', 'mapper', 'csv-mapper', 'parser', 'csv-parser'],
+      packages=['csvmapper']
      )
