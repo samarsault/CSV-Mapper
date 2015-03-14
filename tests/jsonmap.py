8d7362bf4b013ab0bc889a591107cf0cf1f00107
Samarjeet <singh.samarjeet.27@gmail.com>
2015-03-14 18:56:29 +0530

    Add tests and travis-ci support

---
+++ tests/jsonmap.py
@@ -0,0 +1,23 @@
+# same test using a JSONMapper
+import os
+import unittest
+import csvmapper
+
+class JSONMapTest(unittest.TestCase):
+
+	def setUp(self):
+		csvFile = os.path.abspath('data/sampleData.csv')
+		mapper = csvmapper.JSONMapper(os.path.abspath('data/mapper2.json'))
+		parser = csvmapper.CSVParser(csvFile, mapper)
+		self.obj = parser.buildObject()
+
+	def test_parse(self):
+		self.assertEqual(self.obj[1].Name, 'Paul')
+
+	def test_type(self):
+		if self.obj != None:
+			aType = type(self.obj[0].Age)
+			self.assertEqual(aType, type(0))
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ tests/test.py
@@ -0,0 +1,23 @@
+# tests type and parsing
+import os
+import unittest
+import csvmapper
+
+class BasicTest(unittest.TestCase):
+
+	def setUp(self):
+		csvFile = os.path.abspath('data/sampleData.csv')
+		mapper = csvmapper.XMLMapper(os.path.abspath('data/mapper1.xml'))
+		parser = csvmapper.CSVParser(csvFile, mapper)
+		self.obj = parser.buildObject()
+
+	def test_parse(self):
+		self.assertEqual(self.obj[1].Name, 'Paul')
+
+	def test_type(self):
+		if self.obj != None:
+			aType = type(self.obj[0].Age)
+			self.assertEqual(aType, type(0))
+
+if __name__ == '__main__':
+	unittest.main()
